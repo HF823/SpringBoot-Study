bc81438fd839fb669098880c98690534437bbc22
HF823 <hf9675@qq.com>
2026-09-22 15:08:50 +0800

    Revert "第 2 周：Spring 核心 —— IoC、DI、AOP"
    This reverts commit 7a5fd8742536d131cdf357939950cd04681d58a8.

--- Second/src/main/java/com/example/demo/todo/CreateTodoRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.todo;
-
-public record CreateTodoRequest(String title) {
-}
--- Second/src/main/java/com/example/demo/todo/TodoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.todo;
-
-public class TodoNotFoundException extends RuntimeException{
-    public TodoNotFoundException(Long id){
-        super("Todo 不存在，id = "+id);
-    }
-}
